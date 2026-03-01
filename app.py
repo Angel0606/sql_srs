092ec113ebd4ba1b3449c472fea0df131e4f04c1
ANGELIQUE MATT <angeliqueetmarie@hotmail.fr>
2026-03-01 18:49:42 +0100

    add ini_db file to have duckdb tables

--- app.py
+++ app.py
@@ -1,60 +1,45 @@
 # pylint: disable=missing-module-docstring
-import io
-
-import duckdb
-import pandas as pd
 import streamlit as st
+import duckdb
 
-CSV = """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
-
-beverages = pd.read_csv(io.StringIO(CSV))
-
-CSV2 = """
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-"""
-food_items = pd.read_csv(io.StringIO(CSV2))
-ANSWER = """
-SELECT *
-FROM beverages
-CROSS JOIN food_items
-"""
-solution_df = duckdb.sql(ANSWER)
+# ANSWER = """
+# SELECT *
+# FROM beverages
+# CROSS JOIN food_items
+# """
+# solution_df = duckdb.sql(ANSWER)
+con = duckdb.connect(database="data/exercices_sql_tables.duckdb",read_only=False)
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review ?",
-        ("Joins", "GroupBy", "Window function"),
+        ("cross_joins", "GroupBy", "window_functions"),
         index=None,
         placeholder="Select a theme...",
     )
-    st.write(f"you selected {option}")
+    st.write(f"you selected {theme}")
+    exercise = con.execute(f"SELECT * FROM memory_state where theme='{theme}'").df()
+    st.write(exercise)
 st.header("enter your code")
+
 query = st.text_area("Write your SQL request here")
-if query:
-    result_df = duckdb.query(query).df()
-    st.dataframe(result_df)
-    try:
-        result = result_df[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError:
-        st.write("Some columns are missing")
+# if query:
+#     result_df = duckdb.query(query).df()
+#     st.dataframe(result_df)
+#     try:
+#         result = result_df[solution_df.columns]
+#         st.dataframe(result.compare(solution_df))
+#     except KeyError:
+#         st.write("Some columns are missing")
 
-tab1, tab2 = st.tabs(["Tables", "Solution"])
-with tab1:
-    st.write("Table : beverages")
-    st.dataframe(beverages)
-    st.write("Table : food_items")
-    st.dataframe(food_items)
-    st.write("Expected")
-    st.dataframe(solution_df)
-
-with tab2:
-    st.write(ANSWER)
+# tab1, tab2 = st.tabs(["Tables", "Solution"])
+# with tab1:
+#     st.write("Table : beverages")
+#     st.dataframe(beverages)
+#     st.write("Table : food_items")
+#     st.dataframe(food_items)
+#     st.write("Expected")
+#     st.dataframe(solution_df)
+#
+# with tab2:
+#     st.write(ANSWER)
